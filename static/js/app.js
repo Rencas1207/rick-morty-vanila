3ae2bf52ac87facde8f5185e6b0332fdc87fd312
Rencas1207 <rencasdag.12@gmail.com>
2021-09-19 15:03:18 -0500

    It's done baby

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,21 @@
-console.log('HOLA QUE ACE XD');
+import API from './api.js';
+import Character from './character.js';
+let currentCharacter = 1;
+const api = new API();
+
+// const $characterContainer = document.querySelector('#character-container');
+const $loadNext = document.querySelector('#load-next');
+
+$loadNext.addEventListener('click', async () => {
+  const characterData = await api.getCharacter(++currentCharacter);
+  // console.log(characterData);
+  new Character(characterData);
+});
+
+async function initialApp(initCharacterId) {
+  const characterData = await api.getCharacter(initCharacterId);
+  // console.log(characterData);
+  new Character(characterData);
+}
+
+initialApp(currentCharacter);
---
+++ static/js/character.js
@@ -0,0 +1,50 @@
+export default class Character {
+  constructor({ name, image, gender, species, status }) {
+    this.name = name;
+    this.image = image;
+    this.gender = gender;
+    this.species = species;
+    this.status = status;
+    this.$characterImageContainer = document.querySelector(
+      '#character-image-container'
+    );
+    this.$characterNameContainer = document.querySelector(
+      '#character-name-container'
+    );
+    this.$characterDescriptionContainer = document.querySelector(
+      '#character-description-container'
+    );
+    this.render();
+  }
+  buildImage() {
+    return `
+      <img class="character-img" src=${this.image} alt="${this.name}">
+   `;
+  }
+  buildName() {
+    return `
+      <div class="character-name">
+        <h2>${this.name}</h2>
+      </div>
+    `;
+  }
+  buildDescription() {
+    return `
+      <div class="character-labels">
+        <h3 class="character-label">Género: ${this.gender}</h3>
+        <h3 class="character-label">Especie: ${this.species}</h3>
+        <h3 class="character-label">Status: ${this.status}</h3>
+      </div>
+    `;
+  }
+  render() {
+    this.$characterImageContainer.innerHTML = this.buildImage();
+    this.$characterNameContainer.innerHTML = this.buildName();
+    this.$characterDescriptionContainer.innerHTML = this.buildDescription();
+  }
+}
+
+// const rick = new Character({
+//   name: 'Rencas Pro',
+//   img: 'http://127.0.0.1:5500/static/img/logo@2x.png',
+// });
